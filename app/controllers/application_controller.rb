5ea3dd3777e7e833b7c28ea03386dbf09a6fc5ab
ChristianPorras <chrisporras1@uchicago.edu>
2020-02-17 06:10:50 +0000

    halfway progress on matching target

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < ActionController::Base
+  def list_users
+    render({:template => "templates/users.html.erb"})
+  end
 
+  def insert_user
+    username = params.fetch("user_input")
+    u = User.new
+    u.username = username
+    u.save
+
+    redirect_to("/users")
+  end
+
+  def update_user
+    user_id = params.fetch(:user_id)
+    username = params.fetch("user_input")
+    u = User.where({:id => user_id}).at(0)
+    u.username = username
+    u.save
+
+    redirect_to("/users/#{username}")
+  end
+
+  def user_details
+    username = params.fetch(:username)
+    @user = User.where({:username => username}).at(0)
+
+    render({:template => "templates/user_details.html.erb"})
+  end
+
+  def list_photos
+    render({:template => "templates/photos.html.erb"})
+  end
+
+  def insert_photo
+
+  end
+
+  def photo_details
+    photo_id = params.fetch(:photo_id)
+    @photo = Photo.where({:id => photo_id}).at(0)
+
+    render({:template => "templates/photo_details.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "application", :action => "list_users"})
+
+  get("/users", {:controller => "application", :action => "list_users"})
+
+  get("/users/:username", {:controller => "application", :action => "user_details"})
+
+  get("/insert_user", {:controller => "application", :action => "insert_user"})
+
+  get("/update_user/:user_id", {:controller => "application", :action => "update_user"})
+
+  get("/photos", {:controller => "application", :action => "list_photos"})
+
+  get("/photos/")
+
+  get("/photos/:photo_id", {:controller => "application", :action => "photo_details"})
+
   
   # ======== Routes for Admin Dashboard =============
   devise_for :admin_users, ActiveAdmin::Devise.config
